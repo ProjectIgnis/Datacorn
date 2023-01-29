df9c2f791cf1f12299265abdd0719cd0c72aab6c
Dylam De La Torre <dyxel04@gmail.com>
2023-01-29 07:29:09 +0100

    allow highdpi in app (fixes low res icon in 4k)

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <QApplication>
+#include <QCoreApplication>
 #include <QTranslator>
 
 #include "gui/main_window.hpp"
 
 int main(int argc, char* argv[])
 {
+	QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
 	QApplication a(argc, argv);
 	MainWindow w;
 	w.show();
