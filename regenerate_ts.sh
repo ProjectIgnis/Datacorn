c86ee6e809b38344d04ef33b2f9fba5ee1e4eaf9
Dylam De La Torre <DyXel04@gmail.com>
2023-04-25 14:57:05 +0200

    Add Linux CI/CD (#8)
    The resulting artifact work out of the box on most platforms that have Qt5.
    
    This PR also enhances the way resource files are handled in the project, specially translations.

--- regenerate_ts.sh
+++ regenerate_ts.sh
@@ -1,3 +1,1 @@
-lupdate @ts_list.txt -no-ui-lines -source-language en -target-language es -ts es.ts
-lrelease es.ts -qm res/es.qm
-touch res/res.qrc
+lupdate @ts_list.txt -no-ui-lines -source-language en -target-language es -ts res/es.ts
