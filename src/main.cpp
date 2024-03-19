2fe66ff599357ff8286c278d9cac7fd85f483afc
Dylam De La Torre <dyxel04@gmail.com>
2024-03-19 21:59:49 +0100

    add dark theme by default for Windows
    pass --system-theme to override it and use the system theme

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,27 @@ int main(int argc, char* argv[])
 	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
 #endif
 	QApplication a(argc, argv);
+#ifdef Q_OS_WIN
+	if(!a.arguments().contains("--system-theme"))
+	{
+		a.setStyle("fusion");
+		QPalette p;
+		p.setColor(QPalette::Window, QColor(52, 52, 52));
+		p.setColor(QPalette::WindowText, Qt::white);
+		p.setColor(QPalette::Base, QColor(43, 43, 43));
+		p.setColor(QPalette::AlternateBase, QColor(52, 52, 52));
+		p.setColor(QPalette::ToolTipBase, Qt::black);
+		p.setColor(QPalette::ToolTipText, Qt::white);
+		p.setColor(QPalette::Text, Qt::white);
+		p.setColor(QPalette::Button, QColor(52, 52, 52));
+		p.setColor(QPalette::ButtonText, Qt::white);
+		p.setColor(QPalette::BrightText, Qt::red);
+		p.setColor(QPalette::Link, QColor(220, 206, 128));
+		p.setColor(QPalette::Highlight, QColor(220, 206, 128));
+		p.setColor(QPalette::HighlightedText, Qt::black);
+		a.setPalette(p);
+	}
+#endif // Q_OS_WIN
 	MainWindow w;
 	w.show();
 	return QApplication::exec();
